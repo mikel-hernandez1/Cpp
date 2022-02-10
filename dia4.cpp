45fc1f545312cb36b038890415e74baa543c962e
mikel-hernandez1 <97599101+mikel-hernandez1@users.noreply.github.com>
2022-02-10 23:28:21 +0100

    Create dia4.cpp
    Concepto puntero.

---
+++ dia4.cpp
@@ -0,0 +1,52 @@
+// Referencias y punteros.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  string juego = "Tetris";
+  string &ocio = juego;
+  int num = 5;
+  //string juegos[5];
+  string* ptrJuego = &juego;
+  int* ptrNum = &num;
+
+
+  /*cout << juego << endl; // Imprime Tetris.
+  cout << ocio << endl; // Imprime Tetris.
+  cout << &ocio << endl; // Imprime la dirección.
+  cout << &juego << endl; // Imprime la dirección.
+  ocio = "pokemon";
+  cout << ocio << endl;
+  cout << juego << endl;
+  cout << &juegos << endl;
+  cout << &juegos[0] << endl;
+  cout << &juegos[1] << endl;
+  cout << &juegos[2] << endl;
+  cout << &juegos[3] << endl;
+  cout << &juegos[4] << endl;
+  */
+
+  cout << "Imprimo el juego: " << juego << endl;
+  cout << "Imprimo el &juego: " << &juego << endl;
+  //cout << "Imprimo el *juego: " << *juego << endl;
+  cout << "Imprimo el ocio: " << ocio << endl;
+  cout << "Imprimo el &ocio: " << &ocio << endl << endl;
+  cout << "Imprimo el ptrJuego: " << ptrJuego << endl;
+  cout << "Imprimo el &ptrJuego: " << &ptrJuego << endl;
+  cout << "Imprimo el *ptrJuego: " << *ptrJuego << endl << endl;
+  cout << "Imprimo el num: " << num << endl;
+  cout << "Imprimo el *ptrNum: " << *ptrNum << endl;
+  cout << "Imprimo el &num: " << &num << endl;
+  cout << "Imprimo el ptrNum: " << ptrNum << endl << endl;
+
+
+
+
+
+
+
+
+
+  return 0;
+}
